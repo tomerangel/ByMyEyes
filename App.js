59f0fda1ceff4900109ab34230daa383e3e5f905
tomerangel <34811567+tomerangel@users.noreply.github.com>
2019-11-24 16:41:47 +0200

    ////

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import LoadingScreen from './screens/LoadingScreen'
 import SignupScreen from './screens/SignupScreen'
 import SigninScreen from './screens/SigninScreen'
 import Barcode from './screens/Barcode'
+import Voice from './screens/VoiceButton'
 import Page from './screens/Page'
 //TODO: import firebase
 import * as firebase from 'firebase'
@@ -29,7 +30,8 @@ const MainNavigator = createStackNavigator(
     Add: { screen: AddNewContact },
     Page: { screen: Page },
     View: { screen: ViewContact },
-    Edit: { screen: EditContact }
+    Edit: { screen: EditContact },
+    // Voice:{screen:Voice},
   },
   {
     defaultNavigationOptions: {
